5ccec594375299c1801fd16639b0483bb9b12cd0
Jeff Evans <reznet@gmail.com>
2012-12-07 00:41:13 -0800

    initial add of notes control

---
+++ MidiMusicTimerViewer/MidiMusicTimerViewer/NotesControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidiMusicTimerViewer
+{
+    public partial class NotesControl : UserControl
+    {
+        public NotesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
